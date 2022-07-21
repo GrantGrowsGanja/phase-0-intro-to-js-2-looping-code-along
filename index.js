9f33aa55fd9e04f1d93e0e05cbaefc68851c02c0
Grant Ours <grant.ours55@gmail.com>
2022-07-21 09:57:01 -0600

    commit tree 14bac2063d9ad39a066b71081f69346ebfaf02ab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+const newArray = [];
+
+function writeCards(arrayOfNames, event){
+    for(let i = 0; i < arrayOfNames.length; i++){
+        newArray[i] = `Thank you, ${arrayOfNames[i]}, for the wonderful ${event} gift!`
+    }
+    return newArray;
+}
+
+function countDown(x){
+    while(x>=0){
+        console.log(x);
+        x=x-1;
+    }
+}
